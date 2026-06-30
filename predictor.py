bb22eb64905fa7fc1d3e14926730ca7617c2070d
Elijah <elijahbramos@gmail.com>
2026-06-30 16:22:47 -0700

    feat: added try/excpt catch blocks, formatted printed results, added edge cases for invalid inputs

--- predictor.py
+++ predictor.py
@@ -5,21 +5,27 @@ import os
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-BASE_URL = "https://api.n2yo.com/rest/v1/satellite/"
+BASE_URL = "https://api.n2yo.com/rest/v1/satellite"
 
 
 def get_iss_passes():
 
     while True:
 
-        satellite_choice = input(
-            "Would you like to select a preset satellite or choose your own satellite ID?\nPress 1 : ISS\nPress 2 : Hubble Space Telescope\nPress 3 : James Webb Space Telescope\nPress 4 : Enter your own NORAD ID\nPress 5 : Exit\n"
-        )
+        try:
+            satellite_choice = int(
+                input(
+                    "Would you like to select a preset satellite or choose your own satellite ID?\nPress 1 : ISS\nPress 2 : Hubble Space Telescope\nPress 3 : James Webb Space Telescope\nPress 4 : Enter your own NORAD ID\nPress 5 : Exit\n"
+                )
+            )
+        except ValueError:
+            print("This is not a valid input. Please try again.")
+            continue
 
         satellites = {
-            1: {"name": "ISS", "id": 25544},
-            2: {"name": "Hubble Space Telescope", "id": 20580},
-            3: {"name": "James Webb Space Telescope", "id": 50463},
+            1: {"id": 25544},
+            2: {"id": 20580},
+            3: {"id": 50463},
         }
 
         if satellite_choice == 4:
@@ -29,12 +35,21 @@ def get_iss_passes():
             sat = {"id": sat_id}
         elif satellite_choice == 5:
             break
-        else:
+        elif satellite_choice in satellites:
             sat = satellites[satellite_choice]
+        else:
+            print("This is not a valid input. Please try again.")
+            continue
 
-        location_choice = input(
-            "Please select the location you would like to know the satellite passes over.\nPress 1 : Los Angeles, CA\nPress 2 : New York City, NY\n Press 3 : London, UK\nPress 4 : Enter your own coordinates\nPress 5 : Exit\n"
-        )
+        try:
+            location_choice = int(
+                input(
+                    "Please select the location you would like to know the satellite passes over.\nPress 1 : Los Angeles, CA\nPress 2 : New York City, NY\nPress 3 : London, UK\nPress 4 : Enter your own coordinates\nPress 5 : Exit\n"
+                )
+            )
+        except ValueError:
+            print("This is not a valid input. Please try again.")
+            continue
 
         locations = {
             1: {"name": "Hawthorne, CA", "lat": 34.0522, "lon": -118.2437, "alt": 0},
@@ -51,12 +66,33 @@ def get_iss_passes():
             loc = {"lon": lon, "lat": lat, "alt": alt}
         elif location_choice == 5:
             break
-        else:
+        elif location_choice in locations:
             loc = locations[location_choice]
+        else:
+            print("This is not a valid input. Please try again.")
+            continue
 
-        url = f"{BASE_URL}/positions/{sat['id']}/{loc['lat']}/{loc['lon']}/{loc['alt']}/{10}&apiKey={API_KEY}"
+        url = f"{BASE_URL}/positions/{sat['id']}/{loc['lat']}/{loc['lon']}/{loc['alt']}/{1}&apiKey={API_KEY}"
         response = requests.get(url)
-        return response.json()
+        data = response.json()
+
+        if "positions" not in data:
+            print("No data returned. Please check your inputs.")
+            continue
+
+        info = data["info"]
+        pos = data["positions"][0]
+        time = datetime.fromtimestamp(data["positions"][0]["timestamp"])
+
+        print(f"""
+        Satellite: {info['satname']}
+        Latitude:   {pos['satlatitude']}°
+        Longitude:  {pos['satlongitude']}°
+        Altitude:   {pos['sataltitude']} km
+        Azimuth:    {pos['azimuth']}°
+        Elevation:  {pos['elevation']}°
+        Time:       {time}
+        """)
 
 
 get_iss_passes()
